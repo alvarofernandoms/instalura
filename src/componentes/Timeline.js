5f87d07f2fa832e8a994d2129dfd14d34fe07d76
Álvaro Fernando <alvarofernandoms@gmail.com>
2017-12-07 19:35:26 -0200

    FIXME: Init login check

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import FotoItem from './FotoItem';
+import Login from './Login';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 export default class Timeline extends Component {
 
@@ -11,21 +13,24 @@ export default class Timeline extends Component {
   }
 
   componentDidMount() {
-    fetch(`http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
-      .then(response => response.json())
-      .then(fotos => {
-        this.setState({
-          fotos: fotos
+    if (localStorage.getItem('auth-token') === null) {
+      console.log('não logado');
+      this.context.history.push('/');
+    } else {
+      fetch(`http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
+        .then(response => response.json())
+        .then(fotos => {
+          this.setState({
+            fotos: fotos
+          });
         });
-      })
+    }
   }
 
   render() {
     return (
       <div className="fotos container">
-        {
-          this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto}/>)
-        }
+        { this.state.fotos.map(foto => <FotoItem key={ foto.id } foto={ foto } />) }
       </div>
       );
   }
