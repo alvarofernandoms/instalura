063eef4aae01e0aa52bbddc8896b36eb69c30884
Álvaro Fernando <alvarofernandoms@gmail.com>
2017-12-21 22:06:03 -0200

    Refactoring comment method - changed to "flux way"

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import FotoItem from './FotoItem';
-import PubSub from 'pubsub-js';
 import { CSSTransitionGroup } from 'react-transition-group' // ES6
 import LogicaTimeline from '../logicas/LogicaTimeline';
 
@@ -16,18 +15,9 @@ export default class Timeline extends Component {
   }
 
   componentWillMount() {
-    PubSub.subscribe('timeline', (topico, fotos) => {
-      this.setState({
-        fotos
-      });
-    });
-    PubSub.subscribe('novos-comentarios', (topico, infoComentario) => {
-      const fotoAchada = this.state.fotos.find(foto => foto.id === infoComentario.fotoId)
-      fotoAchada.comentarios.push(infoComentario.novoComentario);
-      this.setState({
-        fotos: this.state.fotos
-      });
-    });
+    this.logicaTimeline.subscribe(fotos => {
+      this.setState({fotos});
+    })
   }
 
   carregaFotos() {
@@ -38,15 +28,7 @@ export default class Timeline extends Component {
     } else {
       urlPerfil = `http://localhost:8080/api/public/fotos/${this.login}`;
     }
-
-    fetch(urlPerfil)
-      .then(response => response.json())
-      .then(fotos => {
-        this.setState({
-          fotos: fotos
-        });
-        this.logicaTimeline = new LogicaTimeline(fotos);
-      });
+    this.logicaTimeline.lista(urlPerfil);
   }
 
   componentDidMount() {
@@ -65,37 +47,14 @@ export default class Timeline extends Component {
   }
 
   comenta(fotoId, textoCometario) {
-    const requestInfo = {
-      method: 'POST',
-      body: JSON.stringify({
-        texto: textoCometario
-      }),
-      headers: new Headers({
-        'Content-type': 'application/json'
-      })
-    }
-
-    fetch(`http://localhost:8080/api/fotos/${fotoId}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, requestInfo)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Não foi possível comentar');
-        }
-      })
-      .then(novoComentario => {
-        PubSub.publish('novos-comentarios', {
-          fotoId: fotoId,
-          novoComentario
-        });
-      })
+    this.logicaTimeline.comenta(fotoId, textoCometario);
   }
 
   render() {
     return (
       <div className="fotos container">
         <CSSTransitionGroup transitionName="timeline" transitionEnterTimeout={ 500 } transitionLeaveTimeout={ 300 }>
-          { this.state.fotos.map(foto => <FotoItem key={ foto.id } foto={ foto } like={ this.like.bind(this) } comenta={ this.comenta } />) }
+          { this.state.fotos.map(foto => <FotoItem key={ foto.id } foto={ foto } like={ this.like.bind(this) } comenta={ this.comenta.bind(this) } />) }
         </CSSTransitionGroup>
       </div>
       );
--- src/logicas/LogicaTimeline.js
+++ src/logicas/LogicaTimeline.js
@@ -6,6 +6,15 @@ export default class LogicaTimeline {
     this.fotos = fotos;
   }
 
+  lista(urlPerfil) {
+    fetch(urlPerfil)
+      .then(response => response.json())
+      .then(fotos => {
+        this.fotos = fotos;
+        PubSub.publish('timeline', this.fotos);
+      });
+  }
+
   like(fotoId) {
     fetch(`http://localhost:8080/api/fotos/${fotoId}/like?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, {
       method: 'POST'
@@ -29,5 +38,37 @@ export default class LogicaTimeline {
         }
         PubSub.publish('timeline', this.fotos);
       });
+  }
+
+  comenta(fotoId, textoCometario) {
+    const requestInfo = {
+      method: 'POST',
+      body: JSON.stringify({
+        texto: textoCometario
+      }),
+      headers: new Headers({
+        'Content-type': 'application/json'
+      })
+    }
+
+    fetch(`http://localhost:8080/api/fotos/${fotoId}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, requestInfo)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Não foi possível comentar');
+        }
+      })
+      .then(novoComentario => {
+        const fotoAchada = this.fotos.find(foto => foto.id === fotoId)
+        fotoAchada.comentarios.push(novoComentario);
+        PubSub.publish('timeline', this.fotos);
+      })
+  }
+
+  subscribe(callback) {
+    PubSub.subscribe('timeline', (topico, fotos) => {
+      callback(fotos);
+    });
   }
 }
